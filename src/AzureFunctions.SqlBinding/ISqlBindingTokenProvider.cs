59da1dfb9b8c1d576fde29eb48ee935ee099dc25
jayendran arumugam <jayendranarumugam@gmail.com>
2020-10-16 20:42:03 +0530

    added support for MI

---
+++ src/AzureFunctions.SqlBinding/ISqlBindingTokenProvider.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureFunctions.SqlBinding
+{
+	public interface ISqlBindingTokenProvider
+	{
+		Task<string> GetToken(CancellationToken cancellationToken);
+	}
+}
---
+++ src/FunctionApp/SqlBindingTokenProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using AzureFunctions.SqlBinding;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace FunctionApp
+{
+    internal class SqlBindingTokenProvider : ISqlBindingTokenProvider
+    {
+        public async Task<string> GetToken(CancellationToken cancellationToken)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://database.windows.net/", null, cancellationToken);
+            return accessToken;
+        }
+    }
+}
