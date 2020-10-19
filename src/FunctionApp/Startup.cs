c0641eebde63e0d591c9986fc1c0955c92785486
Jayendran Arumugam <25414541+jayendranarumugam@users.noreply.github.com>
2020-10-19 12:08:17 +0530

    Support managed identity (#12)
    * Added with simple logo
    
    * migrated to dotnet core 3.1
    
    * Added GA workflows
    
    * added support for MI

---
+++ src/FunctionApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using AzureFunctions.SqlBinding;
+using FunctionApp;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace FunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // If you want to use an access token in the connection, register an implemenation for the `AzureFunctions.SqlBinding.ISqlBindingTokenProvider`
+            builder.Services.AddTransient<ISqlBindingTokenProvider, SqlBindingTokenProvider>();
+        }
+    }
+}
